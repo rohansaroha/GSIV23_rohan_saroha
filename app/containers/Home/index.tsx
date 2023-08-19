57dcea8bfb0bface29ebbd20605d15568a83d6fb
Rohan Saroha <rohansaroha@Rohans-MacBook-Pro.local>
2023-08-19 15:29:32 +0530

    feat/test: Added jest library for testing

--- app/containers/Home/index.tsx
+++ app/containers/Home/index.tsx
@@ -1,24 +1,9 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query';
-  
+import React from 'react';
+
 export const Home = ()=>{
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>
-      fetch('https://api.github.com/repos/TanStack/query').then(
-        (res) => res.json(),
-      ),
-  });
-      
-  if (isLoading) return 'Loading...';
-      
-  if (error) return 'An error has occurred: ' + error.message;
   return(
     <>
-      <div>HOME</div>
+      <div>Home</div>
     </>
   );
 };
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,22 @@
 import type { V2_MetaFunction } from '@remix-run/node'
 import { Home } from '~/containers/Home'
+import { useQuery } from '@tanstack/react-query'
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'Movies' }, { name: 'Hello', content: 'Welcome to IMDB!' }]
 }
 
 export default function Index() {
-  return <Home />
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json()
+      ),
+  })
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+  return <Home data={data} />
 }
